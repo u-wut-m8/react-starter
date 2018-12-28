3a472daeda8edaaf9fc2429535be192a7817e3fc
Sumanmit Padhi <sumanmit.padhi@outlook.com>
2018-12-28 21:19:51 +0530

    Add state to search bar component.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,13 +2,20 @@ import React,  {Component} from "react";
 
 // const SearchBar = () => <input/>;
 class SearchBar extends Component {
+  /*Whenever we define a class based Component, a state object is automatically created with it.*/
+  constructor(props) {
+    super(props);
+    this.state = {term: ""};                      //Initialize state object before using it.
+  }
+
   /*Class Based Component class requires derived class to override render().*/
   render () {
-    return <input onChange={event => console.log(event.target.value)}/>;
+    return (
+      <div>
+        <input onChange={event => this.setState({term: event.target.value})}/>
+      </div>
+    );
   }
-  // onInputChange(event) {
-  //   console.log(event.target.value);
-  // }
 }
 
 export default SearchBar;
